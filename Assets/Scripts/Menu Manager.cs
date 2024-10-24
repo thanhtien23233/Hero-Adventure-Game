731193929db49411ac1e6bed551a912566d5a985
thanhtien23233 <134988658+thanhtien23233@users.noreply.github.com>
2024-10-24 18:23:06 +0700

    Menu

---
+++ Assets/Scripts/Menu Manager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject howToPlayCanvas; // Reference to the How to Play canvas
+
+    // Function for Start Game button
+    public void StartGame()
+    {
+        // Load scene 1 (replace "Scene1" with your actual scene name)
+        SceneManager.LoadScene("Scene1");
+    }
+
+    // Function for How to Play button
+    public void ShowHowToPlay()
+    {
+        // Set the How to Play canvas active
+        howToPlayCanvas.SetActive(true);
+    }
+
+    // Function for closing the How to Play canvas
+    public void CloseHowToPlay()
+    {
+        // Set the How to Play canvas inactive
+        howToPlayCanvas.SetActive(false);
+    }
+
+    // Function for Quit button
+    public void QuitGame()
+    {
+        // Quit the application
+        Application.Quit();
+
+        // For debugging purposes in the Unity Editor
+
+    }
+}
